46f4736a5c8cb68a068350b24f9f21fc76350c15
kath <katherine.jackson@payzone.co.uk>
2017-07-16 13:03:20 +0100

    adding in multiple rings of shaded colour, using hsl

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,34 +22,33 @@ namespace ColourWheelWpf
     {
         public MainWindow()
         {
-            InitializeComponent();            
+            InitializeComponent();
         }
 
 
         public struct CircleData
         {
-            public double R { get; set; }
-            public Point C { get; set; }
+            public double OuterRadius { get; set; }
+            public double InnerRadius { get; set; }
+            public Point Centre { get; set; }
 
-            public double L { get { return C.X - R; } }
-            public double T { get { return C.Y - R; } }
         }
 
         public CircleData GetOuterCircle(Canvas c)
         {
             CircleData cd = new CircleData()
             {
-                C = new Point(c.ActualWidth / 2, c.ActualHeight / 2)
+                Centre = new Point(c.ActualWidth / 2, c.ActualHeight / 2)
             };
 
-            cd.R = (c.ActualWidth > c.ActualHeight) ? (c.ActualHeight / 2) * 0.9 : cd.R = (c.ActualWidth / 2) * 0.9;
+            cd.OuterRadius = (c.ActualWidth > c.ActualHeight) ? (c.ActualHeight / 2) * 0.9 : cd.OuterRadius = (c.ActualWidth / 2) * 0.9;
 
             return cd;
         }
 
         public Tuple<Point, Point> ArcEndPoints(double r, double start, double sweep, Point centre)
         {
-            int x1 = (int)(r * Math.Cos((start-90) * Math.PI / 180) + centre.X);
+            int x1 = (int)(r * Math.Cos((start - 90) * Math.PI / 180) + centre.X);
             int y1 = (int)(r * Math.Sin((start - 90) * Math.PI / 180) + centre.Y);
 
             int x2 = (int)(r * Math.Cos((start + sweep - 90) * Math.PI / 180) + centre.X);
@@ -60,33 +59,33 @@ namespace ColourWheelWpf
 
         public Path CreateWheelSegment(CircleData cd, double start, double sweep, Brush fillBrush, bool showBorders)
         {
-            int width = 100;
-            Path segmentPath = new Path { Stroke = Brushes.Black, StrokeThickness = showBorders?1:0, Fill = fillBrush };
 
-            var outerEndPoints = ArcEndPoints(cd.R, start, sweep, cd.C);
+            Path segmentPath = new Path { Stroke = Brushes.Black, StrokeThickness = showBorders ? 1 : 0, Fill = fillBrush };
+
+            var outerEndPoints = ArcEndPoints(cd.OuterRadius, start, sweep, cd.Centre);
 
             PathFigure pathFigure = new PathFigure();
             pathFigure.StartPoint = outerEndPoints.Item1;
 
             var arc1 = new ArcSegment
             {
-                Size = new Size(cd.R, cd.R),
+                Size = new Size(cd.OuterRadius, cd.OuterRadius),
                 Point = outerEndPoints.Item2,
                 SweepDirection = SweepDirection.Clockwise
             };
 
-            var innerRadius = Math.Max(cd.R - width, 1);
-            var innerEndPoints = ArcEndPoints(innerRadius, start, sweep, cd.C);
+
+            var innerEndPoints = ArcEndPoints(cd.InnerRadius, start, sweep, cd.Centre);
 
             var line1 = new LineSegment
             {
                 Point = innerEndPoints.Item2
             };
 
-            
+
             var arc2 = new ArcSegment
             {
-                Size = new Size(innerRadius, innerRadius),
+                Size = new Size(cd.InnerRadius, cd.InnerRadius),
                 Point = innerEndPoints.Item1,
                 SweepDirection = SweepDirection.Counterclockwise
             };
@@ -139,21 +138,43 @@ namespace ColourWheelWpf
 
             CircleData cd = GetOuterCircle(WheelCanvas);
 
-            
+
             Brush[] rainbow = { Brushes.Red, Brushes.Orange, Brushes.Yellow, Brushes.Green, Brushes.Blue, Brushes.Indigo, Brushes.Violet };
 
             var startColor = startPicker.SelectedColor.HasValue ? startPicker.SelectedColor.Value : Colors.Red;
             var endColor = endPicker.SelectedColor.HasValue ? endPicker.SelectedColor.Value : Colors.Blue;
 
-            int numSegments = NumSegmentsBox.Value.HasValue ? NumSegmentsBox.Value.Value : 20;
+            int numSegments = NumSegmentsBox.Value.HasValue ? NumSegmentsBox.Value.Value : 12;
+            int numCircles = NumCirclesBox.Value.HasValue ? NumCirclesBox.Value.Value : 3;
+            double startingRadius = cd.OuterRadius;
+            double endingRadius = Math.Max(cd.OuterRadius - 100, 1);
+            double radiusChange = (startingRadius - endingRadius) / numCircles;
+
             double sweep = (360.0 / numSegments);
-            for (int i=0;i< numSegments; ++i)
+            for (int j = 0; j < numCircles; ++j)
             {
-                double start = (360.0 / numSegments) * i;
-                double colorIndex = (360.0 / (numSegments - 1)) * i;
-                SolidColorBrush brush = new SolidColorBrush(InterpolateColor(startColor, endColor, colorIndex / 360));
-                WheelCanvas.Children.Add(CreateWheelSegment(cd, start, sweep, brush, false));
-                //     WheelCanvas.Children.Add(CreateWheelSegment(cd, start, sweep, rainbow[i % rainbow.Length], false));
+                double outerRadius = startingRadius - j * radiusChange;
+                double innerRadius = outerRadius - radiusChange;
+                CircleData thisWheel = new CircleData
+                {
+                    OuterRadius = outerRadius,
+                    InnerRadius = innerRadius,
+                    Centre = cd.Centre
+                };
+                for (int i = 0; i < numSegments; ++i)
+                {
+                    double start = (360.0 / numSegments) * i;
+                    double colorIndex = (360.0 / (numSegments - 1)) * i;
+                    double hue = (double)(i) / numSegments;
+                    double saturation = 1;
+                    double luminosity =(((double)j+1) / numCircles)*.9;// 0.1;
+                    Color hsl = HSLColour.ColorFromHSL(hue, saturation, luminosity);
+                    //SolidColorBrush brush = new SolidColorBrush(InterpolateColor(startColor, endColor, colorIndex / 360));
+                    SolidColorBrush brush = new SolidColorBrush(hsl);
+                    WheelCanvas.Children.Add(CreateWheelSegment(thisWheel, start, sweep, brush, false));
+                    //     WheelCanvas.Children.Add(CreateWheelSegment(cd, start, sweep, rainbow[i % rainbow.Length], false));
+                   
+                }
             }
         }
 
@@ -168,6 +189,11 @@ namespace ColourWheelWpf
         }
 
         private void NumSegmentsChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            DrawColourWheel();
+        }
+
+        private void NumCirclesChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
         {
             DrawColourWheel();
         }
